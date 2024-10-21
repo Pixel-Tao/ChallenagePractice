fa31daf4c03aa744730510ff1cb04a46e1721fca
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-10-21 12:38:37 +0900

    [Q1] Input Rebinding

---
+++ Assets/Scripts/Week2/MyCircle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCircle : MonoBehaviour
+{
+    public void OnSpace()
+    {
+        Debug.Log("Space!");
+    }
+}
